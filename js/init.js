b9fa692712deb828f091d1ae01260a62b962d9f8
monty <velimir.vesselinov@gmail.com>
2017-06-18 13:03:57 -0600

    sidenav fixed

--- js/init.js
+++ js/init.js
@@ -1,6 +1,4 @@
-(function($){
-	$(function(){
-		$('.button-collapse').sideNav();
-		$('.parallax').parallax();
-	}); // end of document ready
-})(jQuery); // end of jQuery name space
+$(document).ready(function(){
+	$('.parallax').parallax();
+	$('.button-collapse').sideNav();
+});
